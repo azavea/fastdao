f061a83f2fe32c955df4198f531047b1225a9b09
Jeff Adams <jadams@azavea.com>
2011-05-25 14:29:09 -0400

    Added another test to the base set, updated git push to attempt to push the tags correctly.

--- releaseToSourceforge.bat
+++ releaseToSourceforge.bat
@@ -47,7 +47,7 @@ if %errorlevel% neq 0 (
 echo Executing scp upload of the release zipfile...
 echo yes | \\lr01\putty\pscp.exe -i "C:\Documents and Settings\hudson\.ssh\id_rsa.ppk" FastDAO_%version%.zip azaveaci,fastdao@web.sourceforge.net:/home/frs/project/f/fa/fastdao
 
-echo Creating release tag.
+echo Converting to editable remote git repo.
 set releasetag=release_%version%
 git remote rm origin
 git remote add origin ssh://azaveaci@fastdao.git.sourceforge.net/gitroot/fastdao/fastdao
@@ -55,12 +55,14 @@ if %errorlevel% neq 0 (
     echo ERROR: Unable to set ssh origin to sourceforge.
     exit /b 9
 )
+echo Creating release tag.
 git tag %releasetag%
 if %errorlevel% neq 0 (
     echo ERROR: Unable to create new release tag.
     exit /b 8
 )
-git push
+echo Pushing tag up to remote repo.
+git push --tags
 if %errorlevel% neq 0 (
     echo ERROR: Unable to push release tag up to sourceforge.
     exit /b 9
