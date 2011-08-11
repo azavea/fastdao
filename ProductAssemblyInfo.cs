8122b3d8e2a5dd89573d1f3044e682c61abc753e
Justin Walgran <jwalgran@azavea.com>
2011-08-11 16:06:08 -0400

    Added a solution-level assembly info file
    All the Azavea.Open.DAO.* assemblies should share the
    same version number so I added an assembly info file
    to contain common attributes like 'Company' and
    'AssemblyVersion'

---
+++ ProductAssemblyInfo.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+
+// NOTE: This assembly info is linked into all the Azavea.Open.DAO projects.
+// This allows all the assemblies to share common attributes.
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Azavea")]
+[assembly: AssemblyCopyright("Copyright © Azavea 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
